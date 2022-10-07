f1e9b4d1e1f1824d526e7a928aff9267f2255464
anuup <anupb44@gmail.com>
2022-10-07 09:43:23 +0530

    authentic app for neork

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import Auth from "./Auth";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>HELLO-APP</h1>
+      <br></br>
+
+      <Auth></Auth>
+    </div>
+  );
+}
+
+export default App;
